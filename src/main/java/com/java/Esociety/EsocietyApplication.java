7a870acd0c7a0dcdd5bfa94e932eaf6b5f9b4e04
eva <tsolomitieva@gmail.com>
2022-09-23 02:15:55 +0300

    trying something

--- src/main/java/com/java/Esociety/EsocietyApplication.java
+++ src/main/java/com/java/Esociety/EsocietyApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
@@ -32,6 +33,7 @@ import javax.sql.DataSource;
 @Configuration
 @ComponentScan
 @AutoConfiguration
+@EnableJpaRepositories
 
 
 public class EsocietyApplication extends WebSecurityConfigurerAdapter{
